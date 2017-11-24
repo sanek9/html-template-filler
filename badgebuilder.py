fe676248d22842623d725c138f2067a0bf6e15d9
sanek9 <you@example.com>
2017-11-24 14:00:40 +0300

    next

---
+++ badgebuilder.py
@@ -0,0 +1,64 @@
+#!/bin/python
+
+import imgkit
+import pdfkit
+from tdi import html as _html
+from tdi.tools import html as _html_tools
+import mysql.connector
+from mysql.connector import Error
+
+import os
+os.chdir(os.path.dirname(os.path.abspath(os.path.normpath(__file__))))
+
+#imgkit.from_url('http://google.com', 'out.jpg')
+
+
+
+class Model(object):
+    def __init__(self, objects, func):
+        self.objects = objects
+        self.func = func
+
+    def render_layout(self, node):
+#        print (type(self.objects))
+#        print(self.objects)
+        node.repeat(self.repeat_layout, self.objects, len(self.objects) - 2)
+
+    def repeat_layout(self, node, obj, last_sep_idx):
+        self.func(node.template, obj)
+	
+
+class BadgeBuilder(object):
+    def _func(self, node, key):
+#        print (type(person))
+#        print(person)
+        person = self.persons[key]
+        node.name.content = person['name']
+        node.surname.content = person['surname']
+        node.patronymic.content = person['patronymic']
+        node.post.content = person['post']
+        node.department.content = person['department']
+        print(person['photo'])
+        node.photo['src'] = person['photo']
+    
+    def make(self, persons, out):
+        self.persons = persons
+#        print (type(persons))
+#        print(persons)
+        tpl = _html.from_files(['layout.html', 'badge.html'])
+#        print dir(tpl)
+        m = tpl.render_string(Model(persons, self._func))
+#        print m
+        with open('tmp.html', 'w') as f:
+            f.write(m)
+
+        options = {
+        #	'format': 'png'
+        #	'width' : '0'
+        }
+
+
+        pdfkit.from_file('tmp.html', 'out.pdf', options=options)
+
+
+#imgkit.from_string('Hello!', 'out.jpg')
--- main.py
+++ main.py
@@ -1,50 +1,166 @@
-#!/bin/python
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
 
-import imgkit
-import pdfkit
-from tdi import html as _html
-from tdi.tools import html as _html_tools
 import mysql.connector
-from mysql.connector import Error
-
 import os
-os.chdir(os.path.dirname(os.path.abspath(os.path.normpath(__file__))))
+from mysql.connector import Error
+from badgebuilder import BadgeBuilder
 
-#imgkit.from_url('http://google.com', 'out.jpg')
+PHOTO_PATH='photo'
 
-
-
-class Model(object):
+class TermUI(object):
     def __init__(self):
-        self.scope_menu = Badge()
+        
+        
+        self._run = True
+        self.comm = {
+                "s" : self.select,
+                "a" : self.append,
+                "e" : self.exit,
+                "p" : self.print_list,
+                "m" : self.make
+            }
+        self.persons = {}
+        
+    def get_connection(self):
+        conn = mysql.connector.connect(host='192.168.21.70',
+                                        database='kadry',
+                                        user='sanek9',
+                                        password='suse100')
+        if conn.is_connected():
+            print('Connected to MySQL database')
+        return conn
+    def make(self):
+        try:
+            conn = self.get_connection();
+            self._save_photo(conn, self.persons)
+        except Error as e:
+            print(e)
 
-    def render_layout(self, node):
-        fruits = [
-                u'apples', u'pears', u'bananas', u'pineapples',
-        ]
-        node.repeat(self.repeat_layout, fruits, len(fruits) - 2)
+        finally:
+            conn.close()
+            
+        bb = BadgeBuilder()
+        
+        
+        #line = raw_input("save path [./out.pdf]: ")
+        bb.make(self.persons,"out.pdf")
+    def select(self):
+        try:
+            conn = self.get_connection();
 
-    def repeat_layout(self, node, fruit, last_sep_idx):
-        node.template.name.content=fruit
-	
+            line = raw_input("find:> ")
+            tmp = self._fetch_persons(conn, "(s.Family like {0}) or (s.Imya like {0}) or (s.Otch like {0});".format("'%"+line+"%'"))
+            
+            self._print_persons(tmp)
+            if(tmp):
+                self._add_to_list(tmp)
+            
+        except Error as e:
+            print(e)
 
+        finally:
+            conn.close()
+    def _print_persons(self, persons):
+        if(persons):
+            for tab, person in persons.items():
+                print(u"|{:>4}|{:>20}|{:>20}|{:>20}|{:>60}|{:>60}|".format(person['tab'], person['name'], person['surname'], person['patronymic'], person['post'], person['department']))
+        else:
+            print("List is empty")
+            
+    def _fetch_persons(self, conn, where):
+        cursor = conn.cursor()
+        cursor.execute("SELECT s.Cod_sostav, s.N_Tab, s.Family, s.Imya, s.Otch, d.Name_dolg, o.Name_otdel \
+            FROM Tsostav s\
+            left join Spr_dolg d on s.Cod_dolg = d.Cod_dolg\
+            left join Spr_otdel o on s.Cod_otdel = o.Cod_otdel\
+            where {};".format(where))
+            
+        row = cursor.fetchone()
+        tmp = {}
+        while row is not None:
+            person = {
+                    'Cod_sostav' : row[0],
+                    'tab' : row[1],
+                    'name' : row[2],
+                    'surname' : row[3],
+                    'patronymic' : row[4],
+                    'post' : row[5],
+                    'department' : row[6],
+                    'photo':None
+                }
+            tmp[person['tab']] = person
+            
+            row = cursor.fetchone()
+        return tmp
+                
+    def append(self):
+        self._add_to_list()
+    def _add_to_list(self, tmp={}):
+        
+        f = True
+        
+        
+        
+        line = raw_input("tabs:> ")
+        
+        ntmp = []
+        for tab in line.split(' '):
+            if(tab in tmp):
+                self.persons[tab] = tmp[tab]
+            else:
+                ntmp.append(tab)
+    
+        if(ntmp):
+            
+            print("Load from database...")
+            try:
+                conn = self.get_connection();
+                p = self._fetch_persons(conn, "s.N_Tab in ({})".format(", ".join(ntmp)))
+                self.persons.update(p)
+            except Error as e:
+                print(e)
 
+            finally:
+                conn.close()
+        
+        print("ok")
+        
+    def _save_photo(self, conn, persons):
+        cursor = conn.cursor()
+        print(persons)
+        print(persons.keys())
+        for n in persons.keys():
+            print n
+        query = "select s.N_Tab, f.Foto from Tsostav s \
+        left join Tfoto f on s.Cod_sostav = f.Cod_sostav\
+        where s.N_Tab in ({})".format( ', '.join(persons.keys()))
+        print (query)
+        cursor.execute(query)
+        row = cursor.fetchone()
+        while row is not None:
+            path = os.path.join(PHOTO_PATH, row[0]+".jpg")
+#            print("path "+path)
+            with open(path, 'wb') as f:
+                f.write(row[1])
+#            print ("row0 "+ row[0])
+            persons[row[0]]['photo'] = path
+            row = cursor.fetchone()
+            
+    def print_list(self):
+        
+        self._print_persons(self.persons)
+        
+    def exit(self):
+        self._run = False
+    
+    def run(self):
+        while self._run:
+            line = raw_input(":> ")
+            try:
+                self.comm[line]()
+            except KeyError as e:
+                print ('Undefined unit: {}'.format(e.args[0]))
 
-
-tpl = _html.from_files(['layout.html', 'badge.html'])
-print dir(tpl)
-m = tpl.render_string(Model())
-print m
-with open('tmp.html', 'w') as f:
-	f.write(m)
-
-options = {
-#	'format': 'png'
-#	'width' : '0'
-}
-
-
-pdfkit.from_file('tmp.html', 'out.pdf', options=options)
-
-
-#imgkit.from_string('Hello!', 'out.jpg')
+ui = TermUI()
+ui.run()
